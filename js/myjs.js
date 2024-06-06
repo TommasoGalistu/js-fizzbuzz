e094e47c7d723029050d9cc27b1fd89121044f87
TommasoGalistu <tommasogalistu92@gmail.com>
2024-06-06 13:43:39 +0200

    modifiche dimensioni row container

--- js/myjs.js
+++ js/myjs.js
@@ -6,7 +6,7 @@ let genitore = document.querySelector('.row')
 //  i multipli di 5 stampi “Buzz”. Per i numeri che sono sia multipli 
 //  di 3 che di 5 stampi “FizzBuzz”.
 
-for(let i=1; i <= 100; i++){
+for(let i = 1; i <= 100; i++){
     let progressivo = i
     
     if(i % 3 === 0 && i % 5 === 0){
